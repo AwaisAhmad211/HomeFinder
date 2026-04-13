faa7c5008ae14442c61e68af7fe44b08df633624
Awais Ahmad <itxawais211@gmail.com>
2026-04-13 11:04:43 +0500

    Cleaned

--- api/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authRouter from './routes/auth.route.js'
-
-const app = express();
-app.use("/api/auth", authRouter)
-
-app.listen(8800,()=> {
-    console.log("Listining on port 8800")
-})
